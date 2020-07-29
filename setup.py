58b31b90b534dbd3b8b3dba46a5afa8f74b9d4cd
André da Silva <andre-da.silva@outlook.com>
2020-07-29 17:08:48 +0200

    Update setup.py files

--- setup.py
+++ setup.py
@@ -1,3 +1,30 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+import os
 
-setup()
+# Allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+setup(
+    name='django-blog',
+    version='0.0.1',
+    description='Django blog',
+    author='André da Silva',
+    author_email='andre-da.silva@outlook.com',
+    include_package_data=True,
+    packages=find_packages(),
+    classifiers=[
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 1.8 ',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3 :: Only',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content'
+    ],
+    python_requires='>=3.6',
+)
